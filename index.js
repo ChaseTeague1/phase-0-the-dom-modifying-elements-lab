28ea1810a9deec4548eabd52e29e178fbb0e9803
ChaseTeague <teaguechase@yahoo.com>
2023-07-23 03:26:51 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+document.querySelector("main#main").remove();
+newHeader.innerHTML = "Chase Teague is the champion";
